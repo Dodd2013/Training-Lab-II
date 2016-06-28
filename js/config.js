11e51d3843e94e0eb3f10cd49eee2d9aeadf02aa
Dodd <291106637@qq.com>
2016-06-28 09:11:12 +0800

    添加attendance和question

--- js/config.js
+++ js/config.js
@@ -1,9 +1,12 @@
-var urlbase="http://127.0.0.1:8001/1606StevePHP/web/"
+var urlbase="http://192.168.199.246:800/1606StevePHP/web/"
 var config={
 	url:{
 		login:urlbase+"login.php",
 		loginstate:urlbase+"loginstate.php",
 		logout:urlbase+"logout.php",
-		course:urlbase+"course.php"
+		course:urlbase+"course.php",
+		checkAttendance:urlbase+"attendance_check.php",
+		addAttendance:urlbase+"attendance_insert.php",
+		addQuestion:urlbase+"questionmake.php"
 	}
 }
--- js/home.js
+++ js/home.js
@@ -32,11 +32,14 @@ var app = angular.module('myApp', ['ngRoute']).config([
             .when('/home', {
                 templateUrl: 'temp/home.html',
                 controller: 'HomeController'
+            }).when('/attendance', {
+                templateUrl: 'temp/attendance.html',
+                controller: 'AttendanceController'
             })
-            .when('/newCourse', {
+            .when('/course', {
                 templateUrl: 'temp/Course.html',
-                controller: 'CourseController'
-                ,resolve:{
+                controller: 'CourseController',
+                resolve:{
               course:['$q',function($q) {
                 var deferred=$q.defer();
                 progress.setProgress(20);
@@ -64,8 +67,44 @@ var app = angular.module('myApp', ['ngRoute']).config([
                 templateUrl: 'temp/about.html',
                 controller: 'AboutController'
             })
+            .when('/addquestion', {
+                templateUrl: 'temp/addquestion.html',
+                controller: 'AddQuestionController'
+            })
+            .when('/question', {
+                templateUrl: 'temp/question.html',
+                controller: 'QuestionController',
+                    resolve:{
+                  question:['$q',function($q) {
+                    var deferred=$q.defer();
+                    progress.setProgress(20);
+                      $.ajax({
+                                type :"get",
+                                async:false,
+                                url : config.url.addQuestion,
+                                dataType : "jsonp",
+                                data: {opt:"showtable"},
+                                success : function(data){
+                                  progress.setProgress(100);
+                                  if(data.msg==='ok'){
+                                    deferred.resolve(data);
+                                  }else{
+                                    alert("Get question fail!");
+                                  }
+                                },
+                                error:function(){
+                                    progress.setProgress(100);
+                                    alert("Can't connet to server!");
+                                }
+                            });
+                      return deferred.promise;
+                  }]
+                }
+            })
             .otherwise('/home')
     }
+
+    
 ]).controller('navCtrl', function($scope) {
       var res=$.ajax({
               type :"get",
@@ -174,31 +213,132 @@ var app = angular.module('myApp', ['ngRoute']).config([
               alert("Can't connet to server!");
           }
       });
-    //   $.ajax({type :"get",async:true,url : config.url.course,dataType : "jsonp",
-    //         jsonpCallback:"success_jsonpCallback",//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
-    //         data: {opt:'showtable'},
-    //         success : function(data){
-    //           progress.setProgress(100);
-    //           if(data.msg==='ok'){
-    //             $scope.$apply(function() {
-    //               $scope.courseList=data;
-    //             });
-    //           }else{
-    //             alert("Get Course error");
-    //           }
-    //         },
-    //         error:function(){
-    //           progress.setProgress(100);
-    //             alert("Can't connet to server!");
-    //         }
-    // });
   };
 
-}])
+}]).controller("AttendanceController",function($scope) {
+  var indexData=new Array();
+  activeThis('#attendanceli');
+  $scope.getAttendance=function() {
+    progress.setProgress(20);
+    var batch=$('#batchInput').val(),course=$('#courseInput').val();
+    $.ajax({
+              type :"get",
+              async:false,
+              url : config.url.checkAttendance,
+              dataType : "jsonp",
+              data: {batch:batch,course:course},
+              success : function(data){
+                $scope.attendance=data;
+                progress.setProgress(100);
+              },
+              error:function(){
+                  alert("Can't connet to server!");
+              }
+          });
+  };
+  $scope.addAttendance=function(index,$event) {
+    var html=$($event.target).html();
+    if(html==='Add'){
+      indexData.push(index);
+      $($event.target).html("+1 Click to Cancel");
+      $($event.target).removeClass("btn-danger");
+      $($event.target).addClass("btn-success");
+    }else{
+      var deleteindex=0
+      for (var i = indexData.length - 1; i >= 0; i--) {
+        if(indexData[i]===index){
+          deleteindex=i;
+          break;
+        }
+      }
+      indexData.splice(deleteindex,1);
+      $($event.target).html("Add");
+      $($event.target).removeClass("btn-success");
+      $($event.target).addClass("btn-danger");
+    }
+  }
+  $scope.sendAttendance=function() {
+    var confirmStr="Follow Student is here:\n";
+    for (var i = indexData.length - 1; i >= 0; i--) {
+      confirmStr+=$scope.attendance.table[indexData[i]].uname+"\n";
+    };
+    if(confirm(confirmStr)){
+      var sendData=Array();
+      var addData=Array();
+      for (var i = indexData.length - 1; i >= 0; i--) {
+        addData=Array();
+        addData[0]=$scope.attendance.table[indexData[i]].uid;
+        addData[1]=$scope.attendance.table[indexData[i]].course;
+        addData[2]=$scope.attendance.table[indexData[i]].batch;
+        sendData.push(addData);
+      };
+      //alert(JSON.stringify(sendData));
+      $.ajax({
+                type :"get",
+                async:false,
+                url : config.url.addAttendance,
+                dataType : "jsonp",
+                data: {values:JSON.stringify(sendData)},
+                success : function(data){
+                  if (data.msg==='ok') {
+                    alert("Save Attendance success!");
+                    indexData=Array();
+                    progress.setProgress(100);
+                    for (var i = indexData.length - 1; i >= 0; i--) {
+                      $scope.attendance.table[indexData[i]]['Attendance Times']++;
+                    };
+                  };
+                },
+                error:function(){
+                    alert("Can't connet to server!");
+                }
+            });
+    }else{
+
+    }
+  };
+})
 .controller("AboutController",function($scope) {
   activeThis('#aboutli');
-});
-
+}).controller("AddQuestionController",function($scope) {
+  activeThis('#addquestionli');
+  $scope.addquestion=function() {
+    var questionId=$("#questionId").val();
+    var question=$("#question").val();
+    var ans1=$("#ans1").val();
+    var ans2=$("#ans2").val();
+    var ans3=$("#ans3").val();
+    var ans4=$("#ans4").val();
+    var correct=$('input[name="correctop"]:checked').val();
+    var questionData=Array();
+    var sendquestionData=Array();
+    questionData.push(questionId,question,ans1,ans2,ans3,ans4,correct);
+    sendquestionData.push(questionData);
+    progress.setProgress(20);
+    $.ajax({
+              type :"get",
+              async:false,
+              url : config.url.addQuestion,
+              dataType : "jsonp",
+              data: {opt:"add",values:JSON.stringify(sendquestionData)},
+              success : function(data){
+                progress.setProgress(100);
+                if(data.msg==='ok'){
+                  alert("Add question success!");
+                }else{
+                  alert("Add question fail!");
+                }
+              },
+              error:function(){
+                  progress.setProgress(100);
+                  alert("Can't connet to server!");
+              }
+          });
+  };
+}).controller("QuestionController",['$scope','question',function($scope,question) {
+  activeThis('#questionli');
+  $scope.question=question;
+}]);
 function logout() {
   var res=$.ajax({
           type :"get",
@@ -224,4 +364,5 @@ function activeCtrl(){
   };
 }
 var activeThis=activeCtrl();
+
 
